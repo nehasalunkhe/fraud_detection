e01f84a0dabc67bcfab4d1848ecf7a8f36324e33
nehasalunkhe <nehasalunkhe27may@gmail.com>
2024-03-12 06:53:36 +0530

    added 3 new ml model code

--- ml_model.py
+++ ml_model.py
@@ -1,2 +1,12 @@
 
 print("Adding all ml related code here")
+
+
+def model1():
+    print("adding code for ML model 1")
+
+def model2():
+    print("adding code for ML model 2")
+
+def model3():
+    print("adding code for ML model 3")
